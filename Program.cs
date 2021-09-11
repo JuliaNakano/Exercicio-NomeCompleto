cc4ff24e21d0e02cd69e238d70f8e9095d811110
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-11 15:20:41 -0300

    Solução do exercício NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio_NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            
+            Console.Write("Digite seu primeiro nome:");
+            nome= Console.ReadLine();
+            
+            Console.Write("Digite seu sobrenome:... ");
+            sobrenome=Console.ReadLine();
+           
+            Console.WriteLine($"{nome} {sobrenome}");
+            Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
+         }
+    }
+}
+            
